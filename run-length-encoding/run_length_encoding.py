a4b93b8597a2077b6e380f11dec8ba7a07acb842
tombiasz <tomasz.m.nowacki@gmail.com>
2017-08-27 19:56:03 +0200

    first version

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -1,6 +1,42 @@
-def decode():
-    pass
+def decode(string):
+    if len(string) == 0:
+        return ''
+
+    mult = ''
+    output = ''
+
+    for c in string:
+        if c.isdigit():
+            mult += c
+        else:
+            if mult != '':
+                mult = int(mult)
+                output += mult * c
+                mult = ''
+            else:
+                output += c
+    return output
 
 
-def encode():
-    pass
+def encode(string):
+    if len(string) == 0:
+        return ''
+
+    char_count = 1
+    last_char = string[0]
+    output = []
+    for c in string[1:]:
+        if c != last_char:
+            if char_count != 1:
+                output.append(str(char_count))
+            output.append(last_char)
+            last_char = c
+            char_count = 1
+        else:
+            char_count += 1
+
+    if char_count != 1:
+        output.append(str(char_count))
+    output.append(last_char)
+
+    return ''.join(output)
