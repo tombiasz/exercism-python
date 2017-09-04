46c1a69ef36b56432fee2061dedfd1c6943b9c60
tombiasz <tomasz.m.nowacki@gmail.com>
2017-09-04 19:34:40 +0200

    anagram solution

---
+++ anagram/anagram.py
@@ -0,0 +1,13 @@
+def anagram_filter(string):
+    string = string.lower()
+    string_chars = sorted(list(string))
+
+    def filter(element):
+        element = element.lower()
+        return element != string and sorted(list(element)) == string_chars
+
+    return filter
+
+
+def detect_anagrams(string, array):
+    return filter(anagram_filter(string), array)
