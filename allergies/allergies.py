8dcee7ef202d9baeb91ea070cc2c2759838f35fd
tombiasz <tomasz.m.nowacki@gmail.com>
2017-09-05 15:31:27 +0200

    allergies solution

---
+++ allergies/allergies.py
@@ -0,0 +1,30 @@
+class Allergies(object):
+
+    ALLERGENTS = {
+        'eggs': 1,
+        'peanuts': 2,
+        'shellfish': 4,
+        'strawberries': 8,
+        'tomatoes': 16,
+        'chocolate': 32,
+        'pollen': 64,
+        'cats': 128,
+    }
+
+    def __init__(self, number):
+        self.number = number
+
+    def _is_allergic_to(self, value):
+        return bool(self.number & value)
+
+    def is_allergic_to(self, string):
+        value = self.ALLERGENTS.get(string)
+        return self._is_allergic_to(value)
+
+    @property
+    def lst(self):
+        return [
+            key
+            for key, value in self.ALLERGENTS.iteritems()
+            if self._is_allergic_to(value)
+        ]
