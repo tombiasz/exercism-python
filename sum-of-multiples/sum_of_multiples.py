61a1eec14164c51369977d0d77310905e258089d
tombiasz <tomasz.m.nowacki@gmail.com>
2017-09-06 21:03:00 +0200

    sum of mutiples solution

---
+++ sum-of-multiples/sum_of_multiples.py
@@ -0,0 +1,6 @@
+def is_divisible(value, dividers):
+    return any(map(lambda divider: value % divider == 0, dividers))
+
+
+def sum_of_multiples(n, dividers):
+    return sum([v for v in xrange(n) if is_divisible(v, dividers)])
