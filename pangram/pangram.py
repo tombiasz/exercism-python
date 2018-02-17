93ddb088368ee77df3b253a429bfaceefe4fc122
tombiasz <tomasz.m.nowacki@gmail.com>
2018-02-17 17:34:04 +0100

    fix: use python3 string.ascii_lowercase

--- pangram/pangram.py
+++ pangram/pangram.py
@@ -3,7 +3,7 @@ import string
 
 def is_pangram(input):
     input = input.lower()
-    for char in string.lowercase:
+    for char in string.ascii_lowercase:
         if char not in input:
             return False
     return True
