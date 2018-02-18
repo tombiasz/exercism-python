a2f4a417d46bbeb82570fdea575ae3a984303e2b
tombiasz <tomasz.m.nowacki@gmail.com>
2018-02-18 20:07:56 +0100

    fix: use python3 str.maketrans in place of string.maketrans

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -15,5 +15,5 @@ def build_alphabet(rotate=0):
 def rotate(plaintext, key):
     alphabet_in = build_alphabet()
     alphabet_out = build_alphabet(rotate=key)
-    translation = string.maketrans(alphabet_in, alphabet_out)
+    translation = str.maketrans(alphabet_in, alphabet_out)
     return plaintext.translate(translation)
