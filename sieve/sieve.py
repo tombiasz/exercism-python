1bea0fb935f718fca39d0d43215ca68ec79b9798
tombiasz <tomasz.m.nowacki@gmail.com>
2018-02-18 20:24:00 +0100

    fix: use range instead of xrange

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -5,9 +5,9 @@ def sieve(n):
     A = [True] * n
     A[0] = False  # 1 is not prime
 
-    for i in xrange(2, int(math.sqrt(n))+1):
+    for i in range(2, int(math.sqrt(n))+1):
         if A[i-1]:
-            for j in xrange(i**2, n+1, i):
+            for j in range(i**2, n+1, i):
                 A[j-1] = False
 
     return [idx + 1 for idx, value in enumerate(A) if value]
